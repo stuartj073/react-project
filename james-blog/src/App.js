53046356a64b41c66a817a1f9cc7395c67e118f2
stuartj073 <stuartj073@gmail.com>
2022-04-20 10:24:55 +0000

    Project set up

---
+++ james-blog/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
